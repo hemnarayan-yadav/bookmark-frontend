12c153f0c842fc9a936846db56b2cf3bee77c872
Hemnarayan <hemnarayanyadav030@gmail.com>
2026-03-30 23:15:16 +0530

    chore: simplify vite configuration by removing unused plugins and updating output directory

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import svgrPlugin from 'vite-plugin-svgr'
-import envCompatible from 'vite-plugin-env-compatible'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [react()],
   build: {
-    outDir: 'build',
+    outDir: 'dist',
   },
- envPrefix: "REACT_APP_",
-  plugins: [
-    react,
-    svgrPlugin({
-          svgrOptions: {
-            icon : true,
-          }
-    })
-  ],
-});
+})
+
